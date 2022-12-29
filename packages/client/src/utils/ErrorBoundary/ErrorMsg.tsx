6db4e847fba37cbfdba9cd5c5f9fa42fceec390e
goliaeth <goliaeth@gmail.com>
2022-12-29 13:38:17 +0500

    fix: add types for ErrorMsg

--- packages/client/src/utils/ErrorBoundary/ErrorMsg.tsx
+++ packages/client/src/utils/ErrorBoundary/ErrorMsg.tsx
@@ -1,10 +1,12 @@
-function ErrorMsg({ error }: any) {
+import { FC } from 'react';
+
+const ErrorMsg: FC<{ error: Error }> = ({ error }) => {
   return (
     <div>
       <p>Something went wrong!</p>
       <p>{error.message}</p>
     </div>
   );
-}
+};
 
 export default ErrorMsg;

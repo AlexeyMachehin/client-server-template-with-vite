4acf57fdb51030d8e7afd9e835d030a1f2f9ec39
goliaeth <goliaeth@gmail.com>
2022-12-29 10:04:57 +0500

    fix: add BrowserRouter to App.test.tsx

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -1,14 +1,19 @@
-import App from './App'
-import { render, screen } from '@testing-library/react'
+import App from './App';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 
-const appContent = 'Вот тут будет жить ваше приложение :)'
+const appContent = 'Вот тут будет жить ваше приложение :)';
 
 // @ts-ignore
 global.fetch = jest.fn(() =>
   Promise.resolve({ json: () => Promise.resolve('hey') })
-)
+);
 
 test('Example test', async () => {
-  render(<App />)
-  expect(screen.findAllByDisplayValue('App')).toBeDefined()
-})
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  expect(screen.findAllByDisplayValue('App')).toBeDefined();
+});

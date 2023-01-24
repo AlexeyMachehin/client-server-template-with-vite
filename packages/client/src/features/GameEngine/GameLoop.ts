f338c26eab9be960ac1f1748c31938968585791e
goliaeth <goliaeth@gmail.com>
2023-01-24 18:20:35 +0500

    feat: add canvas element on GamePage
    and add Screen class to work with canvas, add Screen to GameLoop

--- packages/client/src/features/GameEngine/GameLoop.ts
+++ packages/client/src/features/GameEngine/GameLoop.ts
@@ -1,11 +1,21 @@
+import { Screen } from './Screen';
+
+export type GameLoopPropsType = {
+  width: number;
+  height: number;
+  canvas: HTMLCanvasElement;
+};
+
 export class GameLoop {
   last: number;
   fps: number;
   step: number;
   dt: number;
   now: number;
+  screen: Screen;
 
-  constructor({ width = 500, height = 500 } = {}) {
+  constructor({ width, height, canvas }: GameLoopPropsType) {
+    this.screen = new Screen(width, height, canvas);
     this.last = performance.now();
     this.fps = 60;
     this.step = 1 / this.fps;
@@ -13,27 +23,17 @@ export class GameLoop {
     this.now = 0;
   }
 
-  update(step: number) {
-    console.log('update', step);
-  }
-
-  render(dt: number) {
-    console.log('render', dt);
-  }
-
   tick = () => {
-    console.log('tick');
+    // console.log('tick');
 
     this.now = performance.now();
 
     this.dt = this.dt + (this.now - this.last) / 1000;
     while (this.dt > this.step) {
       this.dt = this.dt - this.step;
-      this.update(this.step); //
     }
     this.last = this.now;
 
-    this.render(this.dt);
     requestAnimationFrame(this.tick);
   };
 

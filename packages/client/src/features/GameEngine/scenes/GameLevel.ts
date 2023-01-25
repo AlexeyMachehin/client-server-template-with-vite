802544ca71d38faa246ea3d4f2081a19b014174d
goliaeth <goliaeth@gmail.com>
2023-01-25 17:35:08 +0500

    feat: add map prerender and camera

--- packages/client/src/features/GameEngine/scenes/GameLevel.ts
+++ packages/client/src/features/GameEngine/scenes/GameLevel.ts
@@ -2,10 +2,15 @@ import { GameLoop } from '../GameLoop';
 import { Scene } from '../Scene';
 import { Sprite } from '../Sprite';
 import { SpriteSheet } from '../SpriteSheet';
+import { TileMap } from '../TileMap';
+import levelOneData from '../maps/level-1.json';
+import { Camera } from '../Camera';
 
 export class GameLevel extends Scene {
   tiles: SpriteSheet;
   stone: Sprite;
+  map: TileMap | undefined;
+  mainCamera: Camera | undefined;
 
   constructor(gameLoop: GameLoop) {
     super(gameLoop);
@@ -20,16 +25,29 @@ export class GameLevel extends Scene {
 
   init(): void {
     super.init();
+    this.map = this.gameLoop.screen.createMap(
+      'level1',
+      levelOneData,
+      this.tiles
+    );
+    this.mainCamera = new Camera({
+      width: this.gameLoop.screen.width,
+      height: this.gameLoop.screen.height,
+      limitX: this.map.width - this.gameLoop.screen.width,
+      limitY: this.map.height - this.gameLoop.screen.height,
+    });
+    // this.mainCamera.watch()
+    this.gameLoop.screen.setCamera(this.mainCamera);
   }
 
   update(step: number): void {
+    this.mainCamera?.update(step);
     super.update(step);
   }
 
   render(dt: number): void {
-    this.gameLoop.screen.fill('#7CFC00');
-    this.gameLoop.screen.print(50, 70, 'Игра началась');
-    this.gameLoop.screen.drawSprite(this.stone);
+    this.gameLoop.screen.fill('#000');
+    this.gameLoop.screen.drawSprite(this.map!);
     super.render(dt);
   }
 }

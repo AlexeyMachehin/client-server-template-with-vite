1569f3665b2b72c3da5a194b401a2ee2ca879ced
goliaeth <goliaeth@gmail.com>
2023-02-01 23:39:23 +0500

    fix: rename dt to deltaTime

--- packages/client/src/features/GameEngine/GameLoop.ts
+++ packages/client/src/features/GameEngine/GameLoop.ts
@@ -10,7 +10,7 @@ export class GameLoop {
   last: number;
   fps: number;
   step: number;
-  dt: number;
+  deltaTime: number;
   now: number;
   screen: Screen;
 
@@ -19,16 +19,17 @@ export class GameLoop {
     this.last = performance.now();
     this.fps = 60;
     this.step = 1 / this.fps;
-    this.dt = 0;
+    this.deltaTime = 0;
     this.now = 0;
   }
 
   tick = () => {
     this.now = performance.now();
 
-    this.dt = this.dt + Math.min(1, (this.now - this.last) / 1000);
-    while (this.dt > this.step) {
-      this.dt = this.dt - this.step;
+    this.deltaTime =
+      this.deltaTime + Math.min(1, (this.now - this.last) / 1000);
+    while (this.deltaTime > this.step) {
+      this.deltaTime = this.deltaTime - this.step;
     }
     this.last = this.now;
 
